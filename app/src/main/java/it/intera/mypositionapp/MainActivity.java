5b547a07a3ec0e727d5684d1ad6774aabe57d63c
aleksej <monsarda@yandex.ru>
2019-05-31 09:47:20 +0300

    init commit

---
+++ app/src/main/java/it/intera/mypositionapp/MainActivity.java
@@ -0,0 +1,13 @@
+package it.intera.mypositionapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

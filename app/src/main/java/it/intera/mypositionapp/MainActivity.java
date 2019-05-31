deef0abb4ffe09d6329d474b781d38b95b5fbd98
aleksej <monsarda@yandex.ru>
2019-05-31 09:59:26 +0300

    stage

--- app/src/main/java/it/intera/mypositionapp/MainActivity.java
+++ app/src/main/java/it/intera/mypositionapp/MainActivity.java
@@ -1,13 +1,37 @@
 package it.intera.mypositionapp;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static String ACTION = "";
+    private BroadcastReceiver broadcastReceiver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initRetrofit();
+
+        BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+
+            }
+        };
+
+        IntentFilter intFilter = new IntentFilter(ACTION);
+        registerReceiver(broadcastReceiver, intFilter);
+
     }
+
+    private void initRetrofit() {
+    }
+
 }
